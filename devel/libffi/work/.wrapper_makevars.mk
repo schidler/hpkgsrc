4d840132216c084b0b8ca89b737b1066cecad009
diger <admin@freenet59.ru>
2012-03-01 15:36:37 +0600

    add devel/flex, devel/libffi, security/PAM, security/heimdal, textproc/aspell-*lang & patch openssh pathname

---
+++ devel/libffi/work/.wrapper_makevars.mk
@@ -0,0 +1,14 @@
+.if !defined(_MAKEVARS_MK)
+_MAKEVARS_MK=	defined
+
+TOOLS_PREFIX.digest=	/boot/common/pkg
+TOOLS_PREFIX.pkg-config=	/boot/common/pkg
+_BLNK_PHYSICAL_PATH.LOCALBASE=	/boot/common/pkg
+_BLNK_PHYSICAL_PATH.WRKDIR=	/boot/develop/hpkgsrc/devel/libffi/work
+_IGNORE_INFO_PATH=	
+_MANCOMPRESSED=	no
+_MANZ=	no
+_USE_TOOLS=	[ awk basename cat chgrp chmod chown cmp cp cut date diff digest dirname echo egrep env expr false file find ftp grep gzcat head hostname id install install-info ln ls mkdir mv patch pkg-config printf pwd rm rmdir sed sh sort tail tar test touch tr true uname wc xargs
+_WRAP_PATH=	/boot/develop/hpkgsrc/devel/libffi/work/.buildlink/bin:/boot/develop/hpkgsrc/devel/libffi/work/.tools/bin:/boot/develop/hpkgsrc/devel/libffi/work/.gcc/bin:/boot/common/pkg/bin:.:/boot/home/config/bin:/boot/common/bin:/bin:/boot/apps:/boot/preferences:/boot/system/apps:/boot/system/preferences:/boot/develop/tools/gnupro/bin:/boot/common/pkg/bin
+
+.endif # _MAKEVARS_MK
