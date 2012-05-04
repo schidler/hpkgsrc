1ccd2b7a89ddabfe3aef8adf5bc14a5aca9c5749
diger <admin@freenet59.ru>
2012-05-05 02:48:42 +0600

    update net/samba to 3.6.3

---
+++ net/samba/patches/patch-libsmb_libsmb__context.c
@@ -0,0 +1,18 @@
+$NetBSD$
+
+--- libsmb/libsmb_context.c.orig	2012-01-29 19:40:43.056098816 +0000
++++ libsmb/libsmb_context.c
+@@ -57,7 +57,12 @@ SMBC_module_init(void * punused)
+     home = getenv("HOME");
+     if (home) {
+         char *conf = NULL;
+-        if (asprintf(&conf, "%s/.smb/smb.conf", home) > 0) {
++	#ifdef __HAIKU__
++		if (asprintf(&conf, "%s/config/settings/smb/smb.conf", home) > 0)
++	#else
++		if (asprintf(&conf, "%s/.smb/smb.conf", home) > 0)
++	#endif
++		 {
+             if (lp_load(conf, True, False, False, True)) {
+                 conf_loaded = True;
+             } else {
