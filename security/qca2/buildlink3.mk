a5b80ff88f245a66432a6c36042577212ebaa089
diger <admin@freenet59.ru>
2012-06-27 11:34:05 +0600

    add security/qca2

---
+++ security/qca2/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.16 2012/05/07 01:53:58 dholland Exp $
+#
+
+BUILDLINK_TREE+=	qca2
+
+.if !defined(QCA2_BUILDLINK3_MK)
+QCA2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qca2+=	qca2>=2.0.0
+BUILDLINK_ABI_DEPENDS.qca2+=	qca2>=2.0.3nb6
+BUILDLINK_PKGSRCDIR.qca2?=	../../security/qca2
+
+.include "../../framework/qt4-libs/buildlink3.mk"
+.include "../../framework/qt4-tools/buildlink3.mk"
+.endif # QCA2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qca2
