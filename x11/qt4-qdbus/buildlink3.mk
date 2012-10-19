a1eb21f6ef6dbf8e7ea45183b87d3be021321dc3
diger <admin@freenet59.ru>
2012-10-20 02:41:14 +0600

    update x11/qt4  to 4.8.3

---
+++ x11/qt4-qdbus/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.15 2012/09/07 19:17:51 adam Exp $
+
+BUILDLINK_TREE+=	qt4-qdbus
+
+.if !defined(QT4_QDBUS_BUILDLINK3_MK)
+QT4_QDBUS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt4-qdbus+=	qt4-qdbus>=4.3.2
+BUILDLINK_ABI_DEPENDS.qt4-qdbus+=	qt4-qdbus>=4.8.3
+BUILDLINK_PKGSRCDIR.qt4-qdbus?=	../../x11/qt4-qdbus
+
+.include "../../x11/qt4-libs/buildlink3.mk"
+.include "../../sysutils/dbus/buildlink3.mk"
+.endif # QT4_QDBUS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt4-qdbus
---
+++ x11/qt4-qscintilla/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.13 2012/09/07 19:17:51 adam Exp $
+
+BUILDLINK_TREE+=	qt4-qscintilla
+
+.if !defined(QT4_QSCINTILLA_BUILDLINK3_MK)
+QT4_QSCINTILLA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt4-qscintilla+=	qt4-qscintilla>=2.4.3
+BUILDLINK_ABI_DEPENDS.qt4-qscintilla+=	qt4-qscintilla>=2.6.2nb2
+BUILDLINK_PKGSRCDIR.qt4-qscintilla?=	../../x11/qt4-qscintilla
+
+.include "../../x11/qt4-libs/buildlink3.mk"
+.endif	# QT4_QSCINTILLA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt4-qscintilla
